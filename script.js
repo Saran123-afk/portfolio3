6a20eb56301cf0fbd1e54f17bcbf8331798cb83e
Saran123-afk <14554@phanthong.ac.th>
2026-07-14 11:44:05 +0700

    Update script.js

--- script.js
+++ script.js
@@ -174,3 +174,17 @@ document.querySelectorAll('a[href^="#"]').forEach(anchor => {
     });
 
 });
+
+window.addEventListener("load",()=>{
+
+const loader=document.getElementById("loader");
+
+loader.style.opacity="0";
+
+setTimeout(()=>{
+
+loader.style.display="none";
+
+},800);
+
+});
